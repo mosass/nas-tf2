09cd6fc40885e187ef01f406ee9d3f4d2b76ad40
Phanomphon Yotchon <phanomphon.yotchon@gmail.com>
2020-02-26 04:32:57 +0700

    Update

--- lib/model.py
+++ lib/model.py
@@ -3,6 +3,7 @@ from lib.spec import Spec
 from lib.cell import Cell
 import lib.base_ops as ops
 import numpy as np
+import time
 
 
 class Stack(object):
@@ -47,5 +48,57 @@ class NModel(object):
       tensors = Stack(self.spec, tensors, name=("%s-%d" % (self.name, s))).build(downsample=(s > 0))
     
     tensors = tf.reduce_mean(tensors, [1,2])
-    tensors = self.dense(tensors)
-    return tensors
+    self.outputs = self.dense(tensors)
+
+    self.model = tf.keras.Model(self.inputs, self.outputs, name=self.name)
+
+    return self.model
+  
+  def train_and_evaluate(self):
+    optimizer = tf.keras.optimizers.RMSprop(
+            learning_rate=tf.keras.experimental.CosineDecay(0.2, 256, alpha=0.0),
+            momentum=0.9,
+            epsilon=1.0)
+    self.model.compile(optimizer=optimizer,
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+    (train_validate_images, train_validate_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
+
+    train_images, validate_images = np.split(train_validate_images, [int(.8 * len(train_validate_images))])
+    train_labels, validate_labels = np.split(train_validate_labels, [int(.8 * len(train_validate_labels))])
+
+    train_images, validate_images, test_images = train_images / 255.0, validate_images / 255.0,test_images / 255.0
+    
+    time_his = TimeHistory()
+    
+    history = self.model.fit(train_images, train_labels, epochs=4, batch_size=256,
+                  callbacks=[time_his],
+                  validation_data=(validate_images, validate_labels))
+    evaluate = self.model.evaluate(test_images, test_labels)
+
+    data = {
+      "trainable_parameters": np.sum([np.prod(v.get_shape().as_list()) for v in self.model.trainable_variables]),
+      "params": history.params,
+      "history": history.history,
+      "training_time": time_his.training_time,
+      "train_accuracy": history.history["accuracy"][-1],
+      "validation_accuracy": history.history["val_accuracy"][-1],
+      "test_accuracy": evaluate[1],
+    }
+
+    return data
+
+class TimeHistory(tf.keras.callbacks.Callback):
+  def on_train_begin(self, logs={}):
+    self.times = []
+    self.start_time = time.time()
+
+  def on_epoch_begin(self, batch, logs={}):
+    self.epoch_time_start = time.time()
+
+  def on_epoch_end(self, batch, logs={}):
+    self.times.append(time.time() - self.epoch_time_start)
+
+  def on_train_end(self, logs={}):
+    self.training_time = time.time() - self.start_time
